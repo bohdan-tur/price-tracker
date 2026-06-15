4b61d04912e00b7dad1d8bb8e35af022b3c5f0bb
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-06-15 07:14:02 +0300

    feat(auth): add security utilities, dependencies and update user model

---
+++ app/backend/dependencies.py
@@ -0,0 +1,47 @@
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends,HTTPException
+from .db import get_session
+from config import settings
+from jose import jwt,JWTError
+from app.models.user import User
+from sqlalchemy import select
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+db_dependency = Annotated[AsyncSession,Depends(get_session)]
+
+
+async def get_current_user(db:db_dependency,token:Annotated[str,Depends(oauth2_scheme)]):
+
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token,settings.SECRET_KEY,algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+            raise credentials_exception
+
+    query = await db.execute(select(User).where(User.email == email))
+    user = query.scalar_one_or_none()
+    if user is None:
+        raise credentials_exception
+
+    if not  user.is_active:
+        raise HTTPException(status_code=403,detail="Inactive user account")
+
+    return user
+
+
+
+
+
+async def get_current_admin_user(user:Annotated[User,Depends(get_current_user)]):
+    if not user.is_superuser:
+        raise HTTPException(status_code=403,detail="Not enough permissions")
+    return user
--- app/models/user.py
+++ app/models/user.py
@@ -8,5 +8,6 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str]
     is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
 
     items = relationship("Item", back_populates="owner")
