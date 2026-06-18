058afa02ad6ecdcb2ef79c054d92a7ee26e3dbbd
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-06-18 22:21:47 +0300

    test(items): mock external scraper in create item test

--- app/tests/test_items.py
+++ app/tests/test_items.py
@@ -1,27 +1,11 @@
 from conftest import async_client
 from httpx import AsyncClient
 from app.models.item import Item
-from app.backend.dependencies import get_current_user
+from unittest.mock import patch
 from app.main import app
-
+from app.backend.dependencies import get_current_user
 from sqlalchemy import select
 from conftest import db_session
-async def test_create_item(async_client:AsyncClient,create_test_user):
-
-    user =  await create_test_user()
-
-    app.dependency_overrides[get_current_user] = lambda: user
-
-    payload = {
-        "title":"Test title",
-         "url": "https://fake-site-example.com/product/123"
-    }
-
-    response = await async_client.post("/items/",json=payload)
-
-    assert response.status_code == 201
-
-
 
 
 async def test_get_items(async_client:AsyncClient,create_test_user):
@@ -34,6 +18,7 @@ async def test_get_items(async_client:AsyncClient,create_test_user):
     assert response.status_code == 200
     assert len(response.json()) == 0
 
+    app.dependency_overrides.pop(get_current_user)
 
 
 async def test_delete_item(async_client:AsyncClient,create_test_user,db_session):
@@ -68,3 +53,29 @@ async def test_delete_item(async_client:AsyncClient,create_test_user,db_session)
 
 
 
+
+async def test_create_item_success(async_client, create_test_user):
+
+    user = await create_test_user()
+    app.dependency_overrides[get_current_user] = lambda: user
+
+
+    payload = {
+        "title": "Iphone 17",
+        "url": "https://rozetka.com.ua/ua/monitor_123/p12345/"
+    }
+
+
+    with patch("app.routers.item.get_current_price", return_value=1500.0):
+
+        response = await async_client.post("/items/", json=payload)
+
+
+    assert response.status_code == 201
+
+
+    data = response.json()
+    assert data["title"] == "Iphone 17"
+    assert "id" in data
+
+    app.dependency_overrides.clear()
