dd7c82458742730cea4148dbe7ea2e9fd77aa919
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-06-19 16:15:43 +0300

    test(scraper): add scraper tests

---
+++ app/tests/test_scraper.py
@@ -0,0 +1,52 @@
+from unittest.mock import patch,AsyncMock
+from app.backend.scraper import get_current_price
+
+import pytest
+import httpx
+from app.backend.scraper import get_current_price
+
+
+async def test_get_current_price_success(httpx_mock):
+    fake_html = """
+    <html>
+        <body>
+            <span class="price__value">1500.00</span>
+        </body>
+    </html>
+    """
+
+
+    httpx_mock.add_response(
+        url="https://rozetka.com.ua/fake_item/",
+        text=fake_html,
+        status_code=200
+    )
+
+
+    price = await get_current_price("https://rozetka.com.ua/fake_item/")
+
+    assert price == 1500.0
+
+
+async def test_get_current_price_not_found(httpx_mock):
+
+    httpx_mock.add_response(
+        url="https://rozetka.com.ua/fake_item/",
+        text="<html><body><h1>Product not found</h1></body></html>"
+    )
+
+    price = await get_current_price("https://rozetka.com.ua/fake_item/")
+
+    assert price is None
+
+
+async def test_get_current_price_network_error(httpx_mock):
+
+    httpx_mock.add_exception(
+        httpx.ReadTimeout("Connection timeout"),
+        url="https://rozetka.com.ua/fake_item/"
+    )
+
+    price = await get_current_price("https://rozetka.com.ua/fake_item/")
+
+    assert price is None
