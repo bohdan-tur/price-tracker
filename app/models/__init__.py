20e8cb9e5183ab29c2f971dbea351e243f1edb4d
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-03 20:59:31 +0300

    fix: fix Base import path

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from app.backend.db import Base
+from app.database.db import Base
 from app.models.item import Item
 from app.models.price_history import PriceHistory
 from app.models.user import User
